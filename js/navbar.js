8dd8660d32a2ea0b43d75797caf4180b3abe1ac6
Shimi Zhang <zhangshimi@gmail.com>
2014-03-03 21:24:49 -0600

    navbar up

--- js/navbar.js
+++ js/navbar.js
@@ -1,15 +1,19 @@
 $(function() {
     if (loaderhtmlfilename.indexOf("activity") >= 0) {
         $("#activity-navbar").addClass("active");
+        $("#show-current").text("Activities");
         //        console.log("active finished");
     }
     if (loaderhtmlfilename.indexOf("repair") >= 0) {
         $("#repair-navbar").addClass("active");
+        $("#show-current").text("Repair");
     }
     if (loaderhtmlfilename.indexOf("meals") >= 0) {
         $("#meals-navbar").addClass("active");
+        $("#show-current").text("Meals");
     }
     if (loaderhtmlfilename.indexOf("profile") >= 0) {
         $("#profile-navbar").addClass("active");
+        $("#show-current").text("Profile");
     }
 });
